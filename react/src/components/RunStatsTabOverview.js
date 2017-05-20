20e58e57ae70d1aa9dbc4d39e0afaab3a6531756
Petr Cimprich <pcimprich@gmail.com>
2017-05-20 23:08:31 +0200

    Tables with individual and aggregated values

--- react/src/components/RunStatsTabOverview.js
+++ react/src/components/RunStatsTabOverview.js
@@ -8,9 +8,24 @@ class RunStatsTabOverview extends React.Component {
   	}
 	
  	render() {
+		const data = this.props.runs.grouped.month;
+	 	const oInd = {}; // each individual month
+		const oAgg = {}; // aggregated since the beginning of year
+	 	for (let y of Object.keys(this.props.runs.grouped.year)) {
+			oInd[y] = {};
+			oAgg[y] = {};
+			oInd[y].m1 = data[y + 'm1'] ? data[y + 'm1'].distance : '-';
+			oAgg[y].m1 = data[y + 'm1'] ? data[y + 'm1'].distance : 0;
+			for (let i = 2; i <= 12; i++) {
+				oInd[y]['m' + i] = data[y + 'm' + i] ? data[y + 'm' + i].distance : '-';
+				oAgg[y]['m' + i] = data[y + 'm' + i] ? data[y + 'm' + i].distance + oAgg[y]['m' + (i-1)] : oAgg[y]['m' + (i-1)];
+			}
+	 	}
+		
 		return (
 			<div className="tabContent">
-				<RunStatsTabOverviewTable runs={this.props.runs} />
+				<RunStatsTabOverviewTable overview={oInd} />
+				<RunStatsTabOverviewTable overview={oAgg} />
 			</div>
 		);
 	}
--- react/src/components/RunStatsTabOverviewTable.js
+++ react/src/components/RunStatsTabOverviewTable.js
@@ -2,20 +2,6 @@ var React = require('react');
 var prettyPeriod = require('../utils/auxiliary.js').prettyPeriod;
 
 const RunStatsTabOverviewTable = (props) => {
-	
-	const data = props.runs.grouped.month;
- 	const oInd = {}; // each individual month
-	const oAgg = {}; // aggregated since the beginning of year
- 	for (let y of Object.keys(props.runs.grouped.year)) {
-		oInd[y] = {};
-		oAgg[y] = {};
-		oInd[y].m1 = data[y + 'm1'] ? data[y + 'm1'].distance : '-';
-		oAgg[y].m1 = data[y + 'm1'] ? data[y + 'm1'].distance : 0;
-		for (let i = 2; i <= 12; i++) {
-			oInd[y]['m' + i] = data[y + 'm' + i] ? data[y + 'm' + i].distance : '-';
-			oAgg[y]['m' + i] = data[y + 'm' + i] ? data[y + 'm' + i].distance + oAgg[y]['m' + (i-1)] : oAgg[y]['m' + (i-1)];
-		}
- 	}
 
 	return (
         <table className="table table-bordered">
@@ -34,26 +20,25 @@ const RunStatsTabOverviewTable = (props) => {
 					<th>Oct</th>
 					<th>Nov</th>
 					<th>Dec</th>
-					<th>total</th>
   	        	</tr>
   		  	</thead>
   		  	<tbody>
-	  			{Object.keys(oInd).map((year, index) => {
+	  			{Object.keys(props.overview).map((year, index) => {
 					return (
 						<tr key={year}>
 							<td>{year}</td>
-							<td>{oInd[year].m1}</td>
-							<td>{oInd[year].m2}</td>
-							<td>{oInd[year].m3}</td>
-							<td>{oInd[year].m4}</td>
-							<td>{oInd[year].m5}</td>
-							<td>{oInd[year].m6}</td>
-							<td>{oInd[year].m7}</td>
-							<td>{oInd[year].m8}</td>
-							<td>{oInd[year].m9}</td>
-							<td>{oInd[year].m10}</td>
-							<td>{oInd[year].m11}</td>
-							<td>{oInd[year].m12}</td>
+							<td>{props.overview[year].m1}</td>
+							<td>{props.overview[year].m2}</td>
+							<td>{props.overview[year].m3}</td>
+							<td>{props.overview[year].m4}</td>
+							<td>{props.overview[year].m5}</td>
+							<td>{props.overview[year].m6}</td>
+							<td>{props.overview[year].m7}</td>
+							<td>{props.overview[year].m8}</td>
+							<td>{props.overview[year].m9}</td>
+							<td>{props.overview[year].m10}</td>
+							<td>{props.overview[year].m11}</td>
+							<td>{props.overview[year].m12}</td>
 						</tr>
 					);
 				   }		
